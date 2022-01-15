62514a8e095022c793c34721a0a166aeec788177
sbencoding <mcbenighost@gmail.com>
2022-01-15 17:21:13 +0100

    refactor: Remove button click sound

--- public/javascripts/buttonSound.js
+++
@@ -1,6 +0,0 @@
-const buttonSound = new Audio("audio/button_click_sound.mp3");
-const buttons = document.querySelectorAll("button");
-
-for(const b of buttons) {
-    b.addEventListener('click', () => buttonSound.play());
-}
